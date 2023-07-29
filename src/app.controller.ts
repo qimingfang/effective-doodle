1744a6364a61f988ce3abea403f00a1f7bba3ef4
Qiming Fang <qf26@cornell.edu>
2023-07-29 18:30:58 -0400

    feat: add error monitoring to application

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from "@nestjs/common";
 import { pick } from "lodash";
+import { InjectSentry, SentryService } from "@travelerdev/nestjs-sentry";
 import packageJSON from "../package.json";
 
 @Controller()
 export class AppController {
+  constructor(@InjectSentry() private readonly client: SentryService) {}
+
   @Get("/status")
   async getStatus() {
     return pick(packageJSON, ["version"]);
+  }
+
+  @Get("/error")
+  async getError() {
+    throw new Error("My first Sentry error!");
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,49 @@
-import { Module } from "@nestjs/common";
+import { HttpException, Module } from "@nestjs/common";
 import { EventEmitterModule } from "@nestjs/event-emitter";
+import { SentryModule, SentryInterceptor } from "@travelerdev/nestjs-sentry";
 import { AppController } from "./app.controller";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+
+console.log(process.env.NODE_ENV);
 
 @Module({
-  imports: [EventEmitterModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      // in production, env variables should be
+      // loaded via environment vars, not .env files
+      ignoreEnvFile: process.env.NODE_ENV === "production",
+    }),
+    SentryModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const NODE_ENV = configService.get("NODE_ENV");
+
+        return {
+          dsn: configService.getOrThrow("SENTRY_DSN"),
+          debug: NODE_ENV !== "production",
+          environment: NODE_ENV === "production" ? "production" : "dev",
+          logLevels: ["debug"], //based on sentry.io loglevel
+        };
+      },
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useFactory: () =>
+        new SentryInterceptor({
+          filters: [
+            {
+              type: HttpException,
+              // Only report 500 errors
+              filter: (exception: any) => 500 > exception.getStatus(),
+            },
+          ],
+        }),
+    },
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
